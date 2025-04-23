93cff8e429aaa4d1455b834827738cf7019fd8d3
Siviwe Kalipa <you@example.com>
2025-04-23 14:05:37 +0200

    Added the help question

--- CyberChatBox/Library.cs
+++ CyberChatBox/Library.cs
@@ -13,14 +13,19 @@ public class Library
             case "what is your purpose":
                 return "I help users stay safe online through awareness.";
             case "what can i ask you about?":
+            case "What can i ask you about?":
                 return "Try topics like password safety, phishing, or safe browsing.";
             case "what is password safety":
+            case "What is password safety":
                 return "Use strong, unique passwords. Avoid reusing them!";
-            case "what is phishing":
+            case "what is phishing?":
+            case "What is Phishing?":
                 return "Be cautious of suspicious emails or links asking for credentials.";
             case "what is safe browsing":
+            case "What is Safe Browsing":
                 return "Stick to trusted websites and look for HTTPS in URLs.";
             case "help":
+            case "Help":
                 return "You can ask me about Passwords, Phishing, Safe browsing or any other Cyber Security tips :)";
             default:
                 return "I'm still learning. Try asking about cybersecurity tips!";
